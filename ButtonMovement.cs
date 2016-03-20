e52395591ed3170cce52499e652ed924a71c005f
Acarin1995 <Acarin1995@gmail.com>
2016-03-20 23:05:53 +0100

    Added files via upload

---
+++ ButtonMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonMovement : TouchManager {
+    public enum type {LeftButton, RightButton, AttackButton };
+    public type buttonType;
+    public GameObject playerObject = null;
+    public GUITexture buttonTexture = null;
+    Rigidbody2D playerRigidbody;
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+        playerRigidbody = playerObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TouchInput(buttonTexture);
+	}
+    void OnFirstTouch()
+    {
+        switch(buttonType)
+        {
+            case type.AttackButton:
+                break;
+            case type.LeftButton:
+                playerObject.transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+                break;
+            case type.RightButton:
+                playerObject.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+                break;
+        }
+    }
+    void OnSecondaryTouch()
+    {
+        switch (buttonType)
+        {
+            case type.AttackButton:
+                break;
+            case type.LeftButton:
+                playerObject.transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+                break;
+            case type.RightButton:
+                playerObject.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+                break;
+        }
+    }
+}
---
+++ TouchManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchManager : MonoBehaviour {
+
+    public static bool guiTouch = false;
+    public void TouchInput(GUITexture texture)
+    {
+        if(texture.HitTest(Input.GetTouch(0).position))
+        {
+            switch(Input.GetTouch(0).phase)
+            {
+                case TouchPhase.Began:
+                    SendMessage("OnFirstTouchBegan");
+                    SendMessage("OnFirstTouch");
+                    guiTouch = true;
+                    break;
+                case TouchPhase.Stationary:
+                    SendMessage("OnFirstTouchStationary");
+                    SendMessage("OnFirstTouch");
+                    guiTouch = true;
+                    break;
+                case TouchPhase.Moved:
+                    SendMessage("OnFirstTouchMoved");
+                    SendMessage("OnFirstTouch");
+                    guiTouch = true;
+                    break;
+                case TouchPhase.Ended:
+                    SendMessage("OnFirstTouchEnded");
+                    SendMessage("OnFirstTouch");
+                    guiTouch = false;
+                    break;
+            }
+        }
+        if (texture.HitTest(Input.GetTouch(1).position))
+        {
+            switch (Input.GetTouch(1).phase)
+            {
+                case TouchPhase.Began:
+                    SendMessage("OnSecondaryTouchBegan");
+                    SendMessage("OnSecondaryTouch");
+                    break;
+                case TouchPhase.Stationary:
+                    SendMessage("OnSecondaryStationary");
+                    SendMessage("OnSecondaryTouch");
+                    break;
+                case TouchPhase.Moved:
+                    SendMessage("OnSecondaryTouchMoved");
+                    SendMessage("OnSecondaryTouch");
+                    break;
+                case TouchPhase.Ended:
+                    SendMessage("OnSecondaryTouchEnded");
+                    SendMessage("OnSecondaryTouch");
+                    break;
+            }
+        }
+
+    }
+}
